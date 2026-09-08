2f0c563b8907c8b018aeb998ed5100f25d26759e
1183265971-ctrl <1183265971@qq.com>
2026-09-09 06:14:20 +0800

    deploy sw.js

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'workbench-v1';
+const ASSETS = ['./', './index.html', './food-db.js', './quotes.js', './manifest.json'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      return cached || fetch(e.request).then(resp => {
+        return resp;
+      }).catch(() => cached);
+    })
+  );
+});
